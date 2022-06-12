f98979d7dfdb10aa99c075097848de3c28b27671
sanket0708 <93979254+sanket0708@users.noreply.github.com>
2022-06-12 18:56:54 -0400

    Time: 0 ms (100.00%), Space: 5.9 MB (17.31%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+       int a = 1;
+        
+        while(num>0)
+        {
+            num = num-a;
+            a = a+2;
+        }
+        
+        return(num==0);
+        
+    }
+};
