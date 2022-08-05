24074a7ec4bfd56610e313d665a2dd3188b5234d
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-08-05 13:10:54 +0530

    Time: 0 ms (100.00%), Space: 7 MB (20.45%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -7,42 +7,42 @@ public:
         
         if(i>0)
         {
-            if(curr[i-1][j]==1) liveneighbours++; //top
+            if(curr[i-1][j]==1 || curr[i-1][j]==3) liveneighbours++; //top
         }
         
         if(i<m-1)
         {
-            if(curr[i+1][j]==1) liveneighbours++; //bottom
+            if(curr[i+1][j]==1 || curr[i+1][j]==3) liveneighbours++; //bottom
         }
         
         if(j>0)
         {
-            if(curr[i][j-1]==1) liveneighbours++; //left
+            if(curr[i][j-1]==1 || curr[i][j-1]==3) liveneighbours++; //left
         }
         
         if(j<n-1)
         {
-            if(curr[i][j+1]==1) liveneighbours++; //right
+            if(curr[i][j+1]==1 || curr[i][j+1]==3) liveneighbours++; //right
         }
         
         //all four diagonals
         
         if(i>0 && j>0)
         {
-            if(curr[i-1][j-1]==1) liveneighbours++;
+            if(curr[i-1][j-1]==1 || curr[i-1][j-1]==3) liveneighbours++;
         }
         
         if(i>0 && j<n-1)
         {
-            if(curr[i-1][j+1]==1) liveneighbours++;
+            if(curr[i-1][j+1]==1 || curr[i-1][j+1]==3) liveneighbours++;
         }
          if(i<m-1 && j>0)
         {
-            if(curr[i+1][j-1]==1) liveneighbours++;
+            if(curr[i+1][j-1]==1 || curr[i+1][j-1]==3) liveneighbours++;
         }
          if(i<m-1 && j<n-1)
         {
-            if(curr[i+1][j+1]==1) liveneighbours++;
+            if(curr[i+1][j+1]==1 || curr[i+1][j+1]==3) liveneighbours++;
         }
         
         return liveneighbours;
@@ -52,7 +52,7 @@ public:
     
     void gameOfLife(vector<vector<int>>& board) {
         
-        vector<vector<int>>curr = board;
+       // vector<vector<int>>curr = board; optimised solution
         int m = board.size();
         int n = board[0].size();
         
@@ -60,22 +60,31 @@ public:
         {
             for(int j=0;j<n;j++)
             {
-                int temp = helper(curr,i,j,m,n);
+                int temp = helper(board,i,j,m,n);
                 
-                if(board[i][j]==0)
+                if(board[i][j]==0) // currently dead
                 {
                     if(temp==3)
                     {
-                        board[i][j] = 1;
+                        board[i][j] = 2; //newly alive
                     }
                 }
                 else if(board[i][j]==1)
                 {
                     if(temp<2 || temp>3)
                     {
-                        board[i][j] = 0;
+                        board[i][j] = 3; //newly dead
                     }
                 }
+            }
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(board[i][j]==2) board[i][j] = 1;
+                else if(board[i][j]==3) board[i][j] = 0;
             }
         }
         
