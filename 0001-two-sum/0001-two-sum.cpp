bca2ef8fd10ca0e7ee04a6d4b9a9f0d846926424
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-11-03 01:18:34 +0530

    Time: 297 ms (41.64%), Space: 10.1 MB (88.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-           vector<int>ans;
+        vector<int>ans;
         
         for(int i=0;i<nums.size();i++)
         {
-            int b = target - nums[i];
+            int diff = target - nums[i];
             
             for(int j=i+1;j<nums.size();j++)
             {
-                if(nums[j]==b)
+                if(nums[j]==diff)
                 {
                     ans.push_back(i);
                     ans.push_back(j);
