c6d86dbcd4e33b37dc361645d88fab286494899d
Sanket Mudholkar <93979254+sanket0708@users.noreply.github.com>
2022-11-04 02:30:28 +0530

    Time: 78 ms (96.51%), Space: 20 MB (52.33%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-           vector<vector<int>>ans;
+        
+        vector<vector<int>>ans;
         int n = nums.size();
-        sort(nums.begin(),nums.end());
         
         if(n<3)
         {
             return {};
         }
         
-        if(nums[0]>0){
+        sort(nums.begin(),nums.end());
+        
+        if(nums[0]>0)
+        {
             return {};
         }
         
         for(int i=0;i<n;i++)
         {
+            int sum = 0;
             if(nums[i]>0)
             {
                 break;
             }
             if(i>0 && nums[i]==nums[i-1]) continue;
-            int sum = 0;
+            
             int j = i+1;
             int k = n-1;
             
             while(j<k)
             {
-                sum = nums[i]+nums[j]+nums[k];
+                sum = nums[i] + nums[j] + nums[k];
                 if(sum<0)
                 {
                     j++;
@@ -39,13 +43,13 @@ public:
                 else
                 {
                     ans.push_back({nums[i],nums[j],nums[k]});
-                    int lastj = nums[j] , lastk = nums[k];
+                    int lastJ = nums[j] , lastK = nums[k];
                     
-                    while(j<k && nums[j]==lastj)
+                    while(j<k && nums[j]==lastJ)
                     {
                         j++;
                     }
-                    while(j<k && nums[k]==lastk)
+                    while(k>j && nums[k]==lastK)
                     {
                         k--;
                     }
